55a77119e71a1a333d7ba1d521580360492a8a37
St0n3M0nKeY <jasonviski@gmail.com>
2020-05-29 15:53:22 +1000

    updated path

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.1.0",
     description="Mine Digital Exchange API version 2 client",
     author="St0n3M0nKeY",
-    url="https://github.com/St0n3M0nKeY/MineDigitalExchangeAPIv2",
+    url="https://github.com/St0n3M0nKeY/MineDigital-APIv2-wrapper.git",
     license = 'MIT',
     packages=[''],
     install_requires = ['requests'],
